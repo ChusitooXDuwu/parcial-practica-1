42f7e55290f1be4090dcb59a47670b2a3b017f46
ChusitooXDuwu <w.chu@uniandes.edu.co>
2025-05-15 13:26:47 +0000

    changes adapted with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost', // Replace with your DB host
+      port: 5432, // Replace with your DB port
+      username: 'postgres', // Replace with your DB username
+      password: 'postgres', // Replace with your DB password
+      database: 'postgres', // Replace with your DB name
+      synchronize: true, // Auto-creates schema. Disable for production.
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
